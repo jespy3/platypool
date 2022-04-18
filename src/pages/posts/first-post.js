48699bdb64c487fd198942552b7b9325e3c6be5f
josh-espinas <josh.espinas@xero.com>
2022-04-18 22:28:36 +1200

    Adds title and profile pic

--- src/pages/posts/first-post.js
+++ src/pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import styles from '../../styles/Home.module.css'
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1 className={styles.title}>
         First Post
       </h1>
